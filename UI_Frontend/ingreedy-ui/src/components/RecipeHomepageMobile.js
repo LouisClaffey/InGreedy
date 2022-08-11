13f1f1c0bc990d24de220ba512e645a9f5d206a6
Louis Claffey <85559012+LouisClaffey@users.noreply.github.com>
2022-08-11 15:35:42 +0100

    container wrap text

--- UI_Frontend/ingreedy-ui/src/components/RecipeHomepageMobile.js
+++ UI_Frontend/ingreedy-ui/src/components/RecipeHomepageMobile.js
@@ -30,18 +30,18 @@ export default function RecipesMobile() {
       {recipes &&
         recipes.map((recipes) => (
           <Item style={{ marginBottom: "2rem" }}>
-            <Grid key={recipes.id} container>
-              <Grid
-                item
-                xs={12}
-                style={{
-                  whiteSpace: "pre-wrap" /* css-3 */,
-                  whiteSpace: "-moz-pre-wrap " /* Mozilla, since 1999 */,
-                  whiteSpace: " -pre-wrap" /* Opera 4-6 */,
-                  whiteSpace: "-o-pre-wrap " /* Opera 7 */,
-                  wordWrap: "break-word",
-                }}
-              >
+            <Grid
+              key={recipes.id}
+              container
+              style={{
+                whiteSpace: "pre-wrap" /* css-3 */,
+                whiteSpace: "-moz-pre-wrap " /* Mozilla, since 1999 */,
+                whiteSpace: " -pre-wrap" /* Opera 4-6 */,
+                whiteSpace: "-o-pre-wrap " /* Opera 7 */,
+                wordWrap: "break-word",
+              }}
+            >
+              <Grid item xs={12}>
                 <Typography
                   variant="h3"
                   style={{
